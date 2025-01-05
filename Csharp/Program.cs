7261888ace3018c121365d6ce7c14917d6dd275f
dongwoo.kim <dongwoo.kim@nm-neo.com>
2025-01-05 16:25:36 +0900

    초기 커밋.

---
+++ Csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp;
+            hp = 100;
+
+            int maxHp = hp;
+
+            string message = $"당신의 HP는 {hp} / {maxHp} 입니다.";
+
+            Console.WriteLine(message);
+        }
+    }
+}
