e1bbeeb1c7ff4cc40148f9c8db67eec838723e32
dongwoo.kim <dongwoo.kim@nm-neo.com>
2025-01-12 19:22:38 +0900

    06섹션 게임진행

--- Csharp/Section05/S05_09_Polymorphism.cs
+++ Csharp/Section05/S05_09_Polymorphism.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Csharp.Section05
-{
-    internal class S05_09_Polymorphism
-    {         
+//namespace Csharp.Section05
+//{
+//    internal class S05_09_Polymorphism
+//    {         
 
-        class Program
-        {
+//        class Program
+//        {
 
-            }
+//            }
 
-            static void Main(string[] args)
-            {
-                string name = "Harry Porter";
+//            static void Main(string[] args)
+//            {
+//                string name = "Harry Porter";
 
-                //1.찾기
-                   bool found = name.Contains("Harry");
-                int index = name.IndexOf('P');
+//                //1.찾기
+//                   bool found = name.Contains("Harry");
+//                int index = name.IndexOf('P');
 
-                //2. 변형
-                name = name + " Junior";
+//                //2. 변형
+//                name = name + " Junior";
 
-                string lowerCaseName = name.ToLower();
-                string upperCaseName = name.ToUpper();
-                string newName = name.Replace('r', 'l');
-
-
-            //3. 분할
-            string[] names = name.Split(new char[] { ' ' });
-            string substringName = name.Substring(5);
+//                string lowerCaseName = name.ToLower();
+//                string upperCaseName = name.ToUpper();
+//                string newName = name.Replace('r', 'l');
 
 
+//            //3. 분할
+//            string[] names = name.Split(new char[] { ' ' });
+//            string substringName = name.Substring(5);
 
 
-        }
-    }
-    }
+
+
+//        }
+//    }
+//    }
 
---
+++ Csharp/Section06/S06_01_Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Section06
+{
+    internal class S06_01_Player
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Game game = new Game();
+
+                while (true)
+                {
+                    game.Process();
+                }
+
+
+
+                //while (true)
+                //{
+
+                //}
+
+                //Player player = new Knight();
+                //Player player2 = new Archer();
+                //Monster monster = new Orc();
+
+                //int damage = player.GetAttack();
+                //monster.OnDamaged(damage);
+
+            }
+
+        }
+    }
+}
