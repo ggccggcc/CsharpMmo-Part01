557deb2dcae4892ba383c8461b3c0ad3d935b517
dongwoo.kim <dongwoo.kim@nm-neo.com>
2025-01-19 16:31:41 +0900

    섹션7_리스트

--- Csharp/Section07/S07_03_Array02.cs
+++ Csharp/Section07/S07_03_Array02.cs
@@ -1,58 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Csharp.Section07
-{
-    internal class S07_03_Array02
-    {
+//namespace Csharp.Section07
+//{
+//    internal class S07_03_Array02
+//    {
 
-        class Map
-        {
-            int[,] tiles =
-            {
-                {1,1,1,1,1 },
-                {1,0,0,0,1 },
-                {1,0,0,0,1 },
-                {1,0,0,0,1 },
-                {1,1,1,1,1 }
-            };
+//        class Map
+//        {
+//            int[,] tiles =
+//            {
+//                {1,1,1,1,1 },
+//                {1,0,0,0,1 },
+//                {1,0,0,0,1 },
+//                {1,0,0,0,1 },
+//                {1,1,1,1,1 }
+//            };
 
-            public void Render()
-            {
-                var defaultColor = Console.ForegroundColor;
+//            public void Render()
+//            {
+//                var defaultColor = Console.ForegroundColor;
 
-                for (int y = 0; y < tiles.GetLength(1); y++)
-                {
-                    for (int x = 0; x < tiles.GetLength(0); x++)
-                    {
-                        if (tiles[x, y] == 1)
-                            Console.ForegroundColor = ConsoleColor.Red;
-                        else
-                            Console.ForegroundColor = ConsoleColor.Green;
+//                for (int y = 0; y < tiles.GetLength(1); y++)
+//                {
+//                    for (int x = 0; x < tiles.GetLength(0); x++)
+//                    {
+//                        if (tiles[x, y] == 1)
+//                            Console.ForegroundColor = ConsoleColor.Red;
+//                        else
+//                            Console.ForegroundColor = ConsoleColor.Green;
 
-                        Console.Write('\u25cf');
-                    }
-                    Console.WriteLine();
-                }
-                Console.ForegroundColor=defaultColor;
-            }
-        }
+//                        Console.Write('\u25cf');
+//                    }
+//                    Console.WriteLine();
+//                }
+//                Console.ForegroundColor=defaultColor;
+//            }
+//        }
 
 
 
-        static void Main(string[] args) { 
-        int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+//        static void Main(string[] args) { 
+//        int[] scores = new int[5] { 10, 30, 40, 20, 50 };
 
-        //int[,] arr = new int[2, 3]; //2층, 호실 3개씩
-            int[,] arr = new int[2, 3] { { 1, 2, 3, }, { 1, 2, 3, } };
+//        //int[,] arr = new int[2, 3]; //2층, 호실 3개씩
+//            int[,] arr = new int[2, 3] { { 1, 2, 3, }, { 1, 2, 3, } };
             
-            Map map = new Map();
-            map.Render();
+//            Map map = new Map();
+//            map.Render();
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
