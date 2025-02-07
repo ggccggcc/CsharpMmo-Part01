b3a9af71a8ef1e6c8b2cdc317ef65437aaf9555d
dongwoo.kim <dongwoo.kim@nm-neo.com>
2025-02-07 21:07:12 +0900

    마지막까지

---
+++ Csharp/Section08/S08_08_Reflection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Section08
+{
+    internal class S08_08_Reflection
+    {
+
+        class Important : System.Attribute
+        {
+            string message;
+
+            public Impontant(string message) { this.message = message; }
+        }
+        class Monster
+        {
+            [Important("Very Important")]
+            public int hp;
+            protected int attack;
+            private float speed;
+
+            void Attack() { }
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Reflection : X-Ray
+
+            Monster monster = new Monster();
+
+            Type type = monster.GetType();
+
+            var fields = type.GetFields(System.Reflection.BindingFlags.Public
+                |System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Static
+                | System.Reflection.BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields) 
+            {
+                string access = "protected";
+                if (field.IsPublic)
+                    access = "public";
+                else if (field.IsPrivate)
+                    access = "private";
+
+                var attributes = field.GetCustomAttributes();
+
+                Console.WriteLine($"{access}{field.FieldType.Name}{field.Name}");
+            }
+        }
+    }
+}
---
+++ Csharp/Section08/S08_09_nullable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Section08
+{
+    internal class S08_09_nullable
+    {
+
+        class Monster
+        {
+            
+        }
+
+        static Monster FindMonster(int id)
+        {
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            Monster monster = FindMonster(101);
+            if(monster != null)
+            {
+
+            }
+
+            int? number = null;
+
+            number = 3;
+
+            int a = number.Value;
+            Console.WriteLine("a");
+        }
+    }
+}
